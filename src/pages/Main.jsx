8d32c4532de424f0f0c9da1bbdf685eeb09378b6
seongho <damadu01@gmail.com>
2023-07-31 22:06:23 +0900

    영화관 디자인 만들기

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,18 +4,41 @@ import Slider from "../components/Main/Slider";
 import SearchInput from "../components/Main/SearchInput";
 import styled from "styled-components";
 
-const MainWrap = styled.div`
-    width: 1440px;
+import bg from "../assets/theater_bg.jpeg";
+
+const MainBg = styled.div`
+    width: 100vw;
+    height: 100vh;
     margin: 0 auto;
-`
+    background: url(${bg}) #000;
+    background-repeat: no-repeat;
+    background-size: 80%;
+    background-position: center center;
+    position: relative;
+    padding: 110px 370px 370px;
+    box-sizing: border-box;
+    .mainWrap{
+        position: absolute;
+        top: 42%; left: 50%;
+        transform: translate(-50%, -50%);
+        width: 70vw;
+        min-width: 700px;
+       aspect-ratio: 16 / 8;
+        margin: 0 auto;
+        background: #fff;
+        overflow-y: scroll;
+    }
+`;
 
 const Main = () => {
     return (
-        <MainWrap >
-            <Header></Header>
-            <Slider />
-            <SearchInput/>
-        </MainWrap>
+        <MainBg>
+            <div className="mainWrap">
+                <Header></Header>
+                <Slider />
+                <SearchInput />
+            </div>
+        </MainBg>
     );
 };
 
