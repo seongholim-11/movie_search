70fc5c079960731e26543258b79ca163c035cb89
seongho <damadu01@gmail.com>
2023-08-01 00:41:59 +0900

    랜덤 이미지 시트

--- src/components/Beginning/Seat.jsx
+++ src/components/Beginning/Seat.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
+import solar from "../../assets/solar.jpg"
+import earth from "../../assets/earth.jpg"
+import galaxy from "../../assets/galaxy.jpg"
+import wormhole from "../../assets/wormhole.jpg"
+import stars from "../../assets/stars.jpg"
 
 const SeatDesign = styled.div`
     width: 30px;
     height: 45px;
     border-radius: 20px 20px 0 0;
     background-color: rgb(7, 0, 58);
-    box-shadow: 0 0 30px 1px rgba(7, 0, 58, 0.7);
+    box-shadow: 0 0 30px 1px rgba(104, 94, 178, 0.7);
     margin: 5px;
     cursor: pointer;
+
     &:hover {
-        background-color: rgb(109, 105, 130);
-        box-shadow: 0 0 30px 1px rgba(109, 105, 130, 0.9);
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+        box-shadow: 0 0 30px 1px rgba(134, 120, 200, 0.9);
     }
 `;
 
+const randomImages = ["", solar, earth, galaxy, wormhole, stars];
+
 const Seat = () => {
-    return <SeatDesign></SeatDesign>;
+    const [backgroundImage, setBackgroundImage] = useState(randomImages[0]);
+
+    const getRandomImage = () => {
+        const randomIndex = Math.floor(Math.random() * randomImages.length);
+        setBackgroundImage(randomImages[randomIndex]);
+    };
+
+    return (
+        <SeatDesign
+            style={{ backgroundImage: `url(${backgroundImage})` }}
+            onMouseEnter={getRandomImage} onMouseLeave={() => setBackgroundImage(randomImages[0])}
+        />
+    );
 };
 
 export default Seat;
