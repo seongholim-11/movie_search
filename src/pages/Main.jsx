7cc9009ed81865ac08984b5320f1357bea794536
seongho <damadu01@gmail.com>
2023-07-31 22:38:57 +0900

    영화관 디자인 완성

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,35 +5,39 @@ import SearchInput from "../components/Main/SearchInput";
 import styled from "styled-components";
 
 import bg from "../assets/theater_bg.jpeg";
+import earth from "../assets/production.mp4";
 
 const MainBg = styled.div`
     width: 100vw;
+    min-width: 700px;
     height: 100vh;
     margin: 0 auto;
-    background: url(${bg}) #000;
+    background: url(${bg}) black;
     background-repeat: no-repeat;
     background-size: 80%;
     background-position: center center;
     position: relative;
-    padding: 110px 370px 370px;
     box-sizing: border-box;
-    .mainWrap{
+
+    .main {
         position: absolute;
-        top: 42%; left: 50%;
+        top: 42%;
+        left: 50%;
         transform: translate(-50%, -50%);
         width: 70vw;
         min-width: 700px;
-       aspect-ratio: 16 / 8;
+        aspect-ratio: 16 / 8;
         margin: 0 auto;
         background: #fff;
         overflow-y: scroll;
+        box-shadow: 0 0 20px 5px rgba(255, 255, 255, 0.7);
     }
 `;
 
 const Main = () => {
     return (
         <MainBg>
-            <div className="mainWrap">
+            <div className="main">
                 <Header></Header>
                 <Slider />
                 <SearchInput />
