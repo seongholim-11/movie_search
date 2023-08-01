96a6e56f151a8950cb769ead927c919cb8f02442
seongho <damadu01@gmail.com>
2023-08-01 21:33:07 +0900

    변경샇랑

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<Beginning/>}/>
+        <Route path={process.env.PUBLIC_URL + '/'} element={<Beginning/>}/>
         <Route path='/main' element={<Main/>}/>
       </Routes>
     </div>
