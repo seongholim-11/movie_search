a7d6badcffe21371147d98d6bdd7e2aa7b8e353b
seongho <damadu01@gmail.com>
2023-08-01 21:07:52 +0900

    배포 오류 수정

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App />
         </Provider>
