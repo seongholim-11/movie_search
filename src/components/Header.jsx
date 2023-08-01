35fca71e6d90823d0ec89350059d8544917dcfc5
seongho <damadu01@gmail.com>
2023-08-01 09:31:41 +0900

    폰트 사이즈 바꾸기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const HeaderStyle = styled.div`
     align-items: center;
     width: 100%;
     height: 70px;
-    font-size: 20px;
+    font-size: 1vw;
     padding: 10px 40px;
     box-sizing: border-box;
     background: rgba(255, 255, 255);
